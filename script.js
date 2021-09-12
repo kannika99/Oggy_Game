218e61f9892cbca719c28219ef1002209438b8db
Kannika Manjunath <47322537+kannika99@users.noreply.github.com>
2021-09-12 18:54:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// variables to access our character and Block
+character = document.getElementById("character");
+block = document.getElementById("block");
+const score = document.getElementById('score');
+
+function jump(){
+  if(character.classList != "animate"){
+    character.classList.add("animate");
+  }
+
+  setTimeout(function(){
+    character.classList.remove("animate");
+  },500);
+}
+
+var checkdead  =setInterval(function(){
+
+    score.innerText++;
+
+  var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+  if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+    block.style.animation = "none";
+    block.style.display = "none";
+
+    alert("YOUR SCORE IS: "+ score.innerText +"🤩! \n\nWanna Play again?");
+    location.reload();
+
+  }
+
+},10);
